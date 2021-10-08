6f421a9e98815d5b4169867642f2481874cb3613
Igor Ugarte <igor.ugarte@upf.edu>
2021-10-08 13:47:37 +0200

    utils.py distances correction

--- week1/utils.py
+++ week1/utils.py
@@ -7,11 +7,11 @@ def euclidean_distance(u,v):
 def l1_distance(u,v):
     return np.linalg.norm((u - v),ord=1)
 
-def chi2_distance(u,v):
-    return np.sum(np.divide(np.power(u-v,2),(u+v)))
+def chi2_distance(u,v):    
+    return np.sum(np.nan_to_num(np.divide(np.power(u-v,2),(u+v))))
 
 def histogram_intersection(u,v):
     return np.sum(np.minimum(u,v))
 
 def hellinger_kernel(u,v):
-    return np.sum(np.sqrt(u,v))
+    return np.sum(np.sqrt(np.multiply(u,v)))
