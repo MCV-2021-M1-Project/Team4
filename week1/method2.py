1d70bb3b7872a4a82420dbb64bd188d50b36c947
Igor Ugarte <igor.ugarte@upf.edu>
2021-10-09 14:26:31 +0200

    method2 comments

--- week1/method2.py
+++ week1/method2.py
@@ -9,40 +9,42 @@ from mapk import mapk
 from utils import euclidean_distance, l1_distance, chi2_distance, histogram_intersection, hellinger_kernel, borders
 
 
+# Read image file
 img_file = '../../data/qsd2_w1/00001.jpg'
 img = cv2.imread(img_file)
 
+# RGB to HSV
 transformed_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 channel1 = transformed_img[:,:,1]
 channel2 = transformed_img[:,:,2]
 
-mask = np.zeros((channel1.shape[0],channel1.shape[1]))
-mask[(channel1[:,:] > 105) | (channel2[:,:] < 40)] = 1
+## Saturation and Value threshold
+thresholded = np.zeros((img.shape[0],img.shape[1]))
+thresholded[(channel1[:,:] > 105) | (channel2[:,:] < 40)] = 1
 
-plt.imshow(mask,cmap='gray')
+plt.imshow(thresholded,cmap='gray')
 plt.show()
 
-upper_border = np.argmax(mask,axis=0)
+# Find Upper and Bottom borders
+upper_border = np.argmax(thresholded,axis=0)
 min,argmin,max,argmax = borders(upper_border)
 
 pointUL = [argmin,min]
 pointUR = [argmax,max]
 
-bottom_border = np.argmax(np.flip(mask),axis=0)
+bottom_border = np.argmax(np.flip(thresholded),axis=0)
 min,argmin,max,argmax = borders(bottom_border)
 
-pointBL = [mask.shape[1] - argmax,mask.shape[0] - max]
-pointBR = [mask.shape[1] - argmin,mask.shape[0] - min]
+pointBL = [img.shape[1] - argmax,img.shape[0] - max]
+pointBR = [img.shape[1] - argmin,img.shape[0] - min]
 
 ## Get the mask
 """ mask = cv2.fillConvexPoly(np.zeros((channel.shape[0],channel.shape[1])),np.array([pointUL,pointUR,pointBR,pointBL]), color=1) """
 
-## Vis
-mask2 = np.zeros((channel1.shape[0],channel1.shape[1]))
-points = np.array([pointUL,pointUR,pointBR,pointBL])
-mask2 = cv2.line(img,pointUL,pointUR, color=255,thickness =5)
-mask2 = cv2.line(mask2,pointUR,pointBR, color=255,thickness =5)
-mask2 = cv2.line(mask2,pointBR,pointBL, color=255,thickness =5)
-mask2 = cv2.line(mask2,pointBL,pointUL, color=255,thickness =5)
-plt.imshow(mask2,cmap='gray')
+## Draw picture's contours
+img_contours = cv2.line(img,pointUL,pointUR, color=255,thickness =5)
+img_contours = cv2.line(img_contours,pointUR,pointBR, color=255,thickness =5)
+img_contours = cv2.line(img_contours,pointBR,pointBL, color=255,thickness =5)
+img_contours = cv2.line(img_contours,pointBL,pointUL, color=255,thickness =5)
+plt.imshow(img_contours,cmap='gray')
 plt.show()
