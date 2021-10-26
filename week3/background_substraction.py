2b132b249c43ae3ad665662f063899feb9092719
davidserra9 <99d.serrano@gmail.com>
2021-10-26 13:08:09 +0200

    bkg substraction for week3

--- week3/background_substraction.py
+++ week3/background_substraction.py
@@ -5,6 +5,7 @@ import numpy as np
 TH_S = 114  # Saturation threshold
 TH_V = 63   # Value threshold
 
+# File in which all the fucntions needed to substract the background are. Either the evaluation functions.
 
 def substractBackground(image):
     """
