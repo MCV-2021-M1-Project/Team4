599cfc1e02efa244adcbf1153932b6bf8d3bcb87
Igor Ugarte <igor.ugarte@upf.edu>
2021-10-28 11:44:03 +0200

    renaming variable and comments

--- week3/read_text.py
+++ week3/read_text.py
@@ -12,31 +12,31 @@ def read_text(img,bbox):
     [left, top, right, bottom] = bbox
     
     text_img = img[top:bottom,left:right]  # Crop image
-    extractedInformation = pytesseract.image_to_string(text_img)
+    extractedText = pytesseract.image_to_string(text_img)
     
     # If we don't find text in the image, we try with a bigger bbox
-    if extractedInformation[:-1] == '':
+    if extractedText[:-1] == '':
         top = top - 5 if top-5 > 0 else 0
         bottom = bottom + 5 if bottom + 5 < img.shape[0] else 0
         text_img = img[top:bottom,left:right]
-        extractedInformation = pytesseract.image_to_string(text_img)
+        extractedText = pytesseract.image_to_string(text_img)
         
-    # If we don't find text in the image, we try with a smaler bbox
-    if extractedInformation[:-1] == '':
+    # If we don't find text in the image, we try with a smaller bbox
+    if extractedText[:-1] == '':
         if bottom - top > 20:
             top = top + 10
             bottom = bottom - 10
             text_img = img[top:bottom,left:right]
-            extractedInformation = pytesseract.image_to_string(text_img)
+            extractedText = pytesseract.image_to_string(text_img)
 
     # Delete extrange chars that never appear in the pictures. Ex.: \n, \r, \x0C, =, %, @, &.
-    extractedInformation = extractedInformation.replace("\n","").replace("\r","").replace("\x0C","").replace("=","").replace("%","").replace("@","").replace("&","")
+    extractedText = extractedText.replace("\n","").replace("\r","").replace("\x0C","").replace("=","").replace("%","").replace("@","").replace("&","")
     
     # Delete  white spaces in the extrem
-    while len(extractedInformation) and extractedInformation[0] == ' ':
-        extractedInformation = extractedInformation[1:]
+    while len(extractedText) and extractedText[0] == ' ':
+        extractedText = extractedText[1:]
     
-    while len(extractedInformation) and extractedInformation[-1] == ' ':
-        extractedInformation = extractedInformation[:-1]
+    while len(extractedText) and extractedText[-1] == ' ':
+        extractedText = extractedText[:-1]
         
-    return extractedInformation
+    return extractedText
