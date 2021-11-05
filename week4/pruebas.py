9e810d71201513c303b39525ee1d4535afb3d23f
davidserra9 <99d.serrano@gmail.com>
2021-11-05 12:41:30 +0100

    pruebas week4

--- week4/pruebas.py
+++ week4/pruebas.py
@@ -19,8 +19,8 @@ gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.SIFT_create()
 
-keypoints_1, descriptors_1 = sift.detectAndCompute(img1, mask1)
-keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+keypoints_1, descriptors_1 = sift.detectAndCompute(gray1, mask1)
+keypoints_2, descriptors_2 = sift.detectAndCompute(gray2, None)
 
 print(f"Number of keypoints bbdd: {len(keypoints_2)}")
 print(f"Number of keypoints qsd1: {len(keypoints_1)}")
@@ -30,7 +30,7 @@ bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 matches = bf.match(descriptors_1, descriptors_2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-print(f"matches: {len(matches)}")
+print(f"matches: {len(matches)}, {len(matches)/len(keypoints_2)}%")
 
 img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
 
@@ -52,8 +52,8 @@ gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.SIFT_create()
 
-keypoints_1, descriptors_1 = sift.detectAndCompute(img1, mask1)
-keypoints_2, descriptors_2 = sift.detectAndCompute(img2, None)
+keypoints_1, descriptors_1 = sift.detectAndCompute(gray1, mask1)
+keypoints_2, descriptors_2 = sift.detectAndCompute(gray2, None)
 
 print(f"Number of keypoints bbdd: {len(keypoints_2)}")
 print(f"Number of keypoints qsd1: {len(keypoints_1)}")
@@ -63,7 +63,7 @@ bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 matches = bf.match(descriptors_1, descriptors_2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-print(f"matches: {len(matches)}")
+print(f"matches: {len(matches)}, {len(matches)/len(keypoints_2)}%")
 
 img3 = cv2.drawMatches(img1, keypoints_1, img2, keypoints_2, matches[:50], img2, flags=2)
 
